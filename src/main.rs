eea84739e0f31d6111abc8b98d2de531739c4e49
shivamsingha <shivamgit@pm.me>
2023-09-01 11:51:32 +0530

    Add templates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
+use askama::Template;
+use askama_axum::IntoResponse;
 use axum::{routing::get, Router};
+use tower_http::services::ServeDir;
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+pub async fn hello() -> Result<impl IntoResponse, std::convert::Infallible> {
+    let hello = HelloTemplate { name: "world" };
+    Ok(hello)
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(hello))
+        .nest_service("/static", ServeDir::new("static"));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
