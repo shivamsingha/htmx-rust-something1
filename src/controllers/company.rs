8ae047c61854821071e9c68a5d06c10aca77d3a5
shivamsingha <shivamgit@pm.me>
2023-09-05 00:12:12 +0530

    Add company and job CRUD

--- src/controllers/company.rs
+++ src/controllers/company.rs
@@ -1,4 +1,4 @@
-use axum::{extract::Path, Extension};
+use axum::{extract::Path, Extension, Form};
 use sqlx::{Pool, Postgres};
 
 use crate::{models::company::Company, templates::CompanyTemplate};
@@ -11,6 +11,56 @@ pub async fn get_company(
         .fetch_one(&conn)
         .await
         .unwrap();
+
+    CompanyTemplate { company }
+}
+
+pub async fn create_company(
+    Form(form_data): Form<Company>,
+    Extension(conn): Extension<Pool<Postgres>>,
+) -> CompanyTemplate {
+    let company = sqlx::query_as!(
+            Company,
+            "INSERT INTO companies (name, description, website, logo_url) VALUES ($1, $2, $3, $4) RETURNING *",
+            form_data.name,
+            form_data.description,
+            form_data.website,
+            form_data.logo_url
+        ).fetch_one(&conn).await.unwrap();
+
+    CompanyTemplate { company }
+}
+
+pub async fn update_company(
+    Path(id): Path<i32>,
+    Form(form_data): Form<Company>,
+    Extension(conn): Extension<Pool<Postgres>>,
+) -> CompanyTemplate {
+    let company = sqlx::query_as!(
+            Company,
+            "UPDATE companies SET name = $1, description = $2, website = $3, logo_url = $4 WHERE id = $5 RETURNING *",
+            form_data.name,
+            form_data.description,
+            form_data.website,
+            form_data.logo_url,
+            id
+        ).fetch_one(&conn).await.unwrap();
+
+    CompanyTemplate { company }
+}
+
+pub async fn delete_company(
+    Path(id): Path<i32>,
+    Extension(conn): Extension<Pool<Postgres>>,
+) -> CompanyTemplate {
+    let company = sqlx::query_as!(
+        Company,
+        "DELETE FROM companies WHERE id = $1 RETURNING *",
+        id
+    )
+    .fetch_one(&conn)
+    .await
+    .unwrap();
 
     CompanyTemplate { company }
 }
--- src/controllers/job.rs
+++ src/controllers/job.rs
@@ -14,3 +14,43 @@ pub async fn get_job(
 
     JobTemplate { job }
 }
+
+pub async fn update_jobs(
+    Path(id): Path<i32>,
+    Extension(conn): Extension<Pool<Postgres>>,
+) -> JobTemplate {
+    let job = sqlx::query_as!(
+        Job,
+        "UPDATE jobs SET expires_at = NOW() WHERE id = $1 RETURNING *",
+        id
+    )
+    .fetch_one(&conn)
+    .await
+    .unwrap();
+
+    JobTemplate { job }
+}
+
+pub async fn create_job(Extension(conn): Extension<Pool<Postgres>>) -> JobTemplate {
+    let job = sqlx::query_as!(
+        Job,
+        "INSERT INTO jobs (title, description, company_id) VALUES ('New Job', 'New Job Description', 1) RETURNING *"
+    )
+    .fetch_one(&conn)
+    .await
+    .unwrap();
+
+    JobTemplate { job }
+}
+
+pub async fn delete_job(
+    Path(id): Path<i32>,
+    Extension(conn): Extension<Pool<Postgres>>,
+) -> JobTemplate {
+    let job = sqlx::query_as!(Job, "DELETE FROM jobs WHERE id = $1 RETURNING *", id)
+        .fetch_one(&conn)
+        .await
+        .unwrap();
+
+    JobTemplate { job }
+}
