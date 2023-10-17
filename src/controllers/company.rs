102f1ace4b071f8b44e1c814afb7925ea2f0213d
shivamsingha <shivamgit@pm.me>
2023-10-17 22:32:02 +0530

    Add multipart data to create_company

--- src/controllers/company.rs
+++ src/controllers/company.rs
@@ -1,4 +1,8 @@
-use axum::{extract::Path, Extension, Form};
+use axum::{
+    extract::{Multipart, Path},
+    Extension, Form,
+};
+
 use sqlx::{Pool, Postgres};
 
 use crate::{models::company::Company, templates::CompanyTemplate};
@@ -16,19 +20,43 @@ pub async fn get_company(
 }
 
 pub async fn create_company(
-    Form(form_data): Form<Company>,
+    mut multipart: Multipart,
     Extension(conn): Extension<Pool<Postgres>>,
-) -> CompanyTemplate {
-    let company = sqlx::query_as!(
-            Company,
-            "INSERT INTO companies (name, description, website, logo_url) VALUES ($1, $2, $3, $4) RETURNING *",
-            form_data.name,
-            form_data.description,
-            form_data.website,
-            form_data.logo_url
-        ).fetch_one(&conn).await.unwrap();
+) -> Result<CompanyTemplate, anyhow::Error> {
+    let mut form_data = Company {
+        id: 0,
+        name: String::new(),
+        description: None,
+        website: None,
+        logo_url: None,
+    };
 
-    CompanyTemplate { company }
+    while let Some(field) = multipart.next_field().await? {
+        let name = field.name();
+        match name {
+            Some("name") => form_data.name = String::from_utf8(field.bytes().await?.to_vec())?,
+            Some("description") => {
+                form_data.description = Some(String::from_utf8(field.bytes().await?.to_vec())?)
+            }
+            Some("website") => {
+                let field_data = field.bytes().await?;
+                let website = std::str::from_utf8(&field_data)?;
+                form_data.website = Some(url::Url::parse(website)?.as_str().to_string());
+            }
+            _ => (), // Ignore other fields
+        }
+    }
+
+    let company = sqlx::query_as!(
+        Company,
+        "INSERT INTO companies (name, description, website, logo_url) VALUES ($1, $2, $3, $4) RETURNING *",
+        form_data.name,
+        form_data.description,
+        form_data.website,
+        form_data.logo_url
+    ).fetch_one(&conn).await.unwrap();
+
+    Ok(CompanyTemplate { company })
 }
 
 pub async fn update_company(
